0af27c7f0fbc54d93651383cf2ba2847c550d471
Tanay <tanaykapoor21@gmail.com>
2025-05-08 12:56:55 +0530

    GO

--- __MACOSX/controllers/-- Connect to the FITBLAST database.sql
+++
@@ -1,9 +0,0 @@
--- Connect to the FITBLAST database
--- Replace placeholders with your actual database credentials
-
-USE FITBLAST;
-
--- Verify connection by listing tables
-SELECT TABLE_NAME
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_TYPE = 'BASE TABLE';
